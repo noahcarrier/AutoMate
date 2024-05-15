2c727a40b1477c4556d45092babb5b8f6ceaa642
Noah Carrier <67935880+noahcarrier@users.noreply.github.com>
2024-05-15 18:03:05 -0400

    committing correct version of hello world page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, Text, View } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
@@ -8,6 +8,7 @@ import { ThemedView } from '@/components/ThemedView';
 export default function HomeScreen() {
   return (
   <View>
-    
+    <Text>Hello, World!</Text>
   </View>  
   );
+}
